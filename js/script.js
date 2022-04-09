c82cc4cfa381bd09a39d850b43a2a2e4fe9ef98d
Sergey Goncharuk <fleshed914@gmail.com>
2022-04-09 16:54:47 +0300

    feat: add mobile menu styles & scripts

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 
 $(document).ready(function(){
 
-
+    let closeMenuButton = $(".header-mobile__close");
+    let openMenuButton = $(".burger");
+    let menuContainer = $(".header-mobile");
+    let menuLinks = $(".header-mobile__link");
+  
+  
+    openMenuButton.click(function() {
+      menuContainer.addClass("header-mobile_active");
+    });
+  
+    closeMenuButton.click(function() {
+      menuContainer.removeClass("header-mobile_active");
+    });
+  
+    menuLinks.click(function() {
+      menuContainer.removeClass("header-mobile_active");
+    });
 
 });
 
